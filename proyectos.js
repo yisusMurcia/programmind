8ec88278b1f93abc1e6eefd7530172096f448409
yisus <yisusmurci@gmail.com>
2023-08-18 12:24:00 -0500

    Cambios preview

--- proyectos.js
+++ proyectos.js
@@ -6,7 +6,7 @@ let myProyects=[
 let proyectsContent= document.getElementById("myProyects");
 let button= document.querySelector(".moreContent");
 myProyects.reverse();
-for (let i = 0; i < 1; i++) {
+for (let i = 0; i < 2; i++) {
     let post= myProyects[i];
     let postTitle= document.createElement("h3");
     postTitle.setAttribute("class", "post")
