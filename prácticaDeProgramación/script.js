0695681723cde688d950c00a40eed6dd8726f621
yisus <yisusmurci@gmail.com>
2023-09-18 18:38:38 -0500

    18 Septiembre 2023 -Se tarmino de agregar el diseño para javascript, el color de acento se aplicará a los elementos con la clase javascript. -Se agregaron explicaciónes a los códigos, estas aparecen al responder correctamente una pregunta.

--- prácticaDeProgramación/script.js
+++ prácticaDeProgramación/script.js
@@ -1,21 +1,28 @@
+//Guardar y eleminar las explicaciónes
+let answers= document.querySelectorAll(".answer");
+let answersExplain= [];
+for (let element of answers){
+    answersExplain.push(element.innerText)
+    element.innerHTML= "";
+};
 let question1= "q1O2";
 let button1= document.querySelector(`.button1`);
-let answer1= document.querySelector(`.answer1`);
+let answer1= document.querySelector(`#práctica1`);
 button1.addEventListener("click",()=>{
     let input= document.querySelector(`#${question1}`);
     if (input.checked){
-        answer1.innerHTML= "Correcto";
+        answer1.innerHTML= "Correcto <br>"+ answersExplain[0];
     }else{
         answer1.innerHTML= "intentalo otra vez";
     };
 });
 let question2= "q2O1";
 let button2= document.querySelector(`.button2`);
-let answer2= document.querySelector(`.answer2`);
+let answer2= document.querySelector(`#práctica2`);
 button2.addEventListener("click",()=>{
     let input= document.querySelector(`#${question2}`);
     if (input.checked){
-        answer2.innerHTML= "Correcto";
+        answer2.innerHTML= "Correcto <br>"+ answersExplain[1];
     }else{
         answer2.innerHTML= "intentalo otra vez";
     };
