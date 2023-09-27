6cfa0637decf26f1db95db286e00b751305e7653
yisus <yisusmurci@gmail.com>
2023-09-26 21:10:38 -0500

    26 septiembre 2023 No.2 -Se corrigió el aspceto del elemento scroll-bar. -Se añadieron los diseños del light mode a la sección del índice y el footer, junto con la creación de sus valores variables.

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ document.documentElement.style.setProperty("--h-color", "orange");
 document.documentElement.style.setProperty("--section-color", "#363636");
 document.documentElement.style.setProperty("--text-color", "white");
 document.documentElement.style.setProperty("--h2-color", "orange");
+document.documentElement.style.setProperty("--a-color", "#cc33cc");
+document.documentElement.style.setProperty("--a-color-hover", "#fa3efa");
+document.documentElement.style.setProperty("--footer-background", "#72b98f");
+document.documentElement.style.setProperty("--news-color", "#994fca");
