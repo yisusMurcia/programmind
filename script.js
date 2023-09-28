080178d64c73d0d624ad6b16f2b8a68d8b5b7388
yisus <yisusmurci@gmail.com>
2023-09-27 21:03:14 -0500

    27 Septeimbre 2023 No.3 -Se añadio el diseño del boton de light mode. -Se añadió el código para pasar a light mode, falta el de regresar.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-//Práctica light mode
-document.documentElement.style.setProperty("--background", "#627e77");
-document.documentElement.style.setProperty("--h-color", "orange");
-document.documentElement.style.setProperty("--section-color", "#363636");
-document.documentElement.style.setProperty("--text-color", "white");
-document.documentElement.style.setProperty("--h2-color", "orange");
-document.documentElement.style.setProperty("--a-color", "#cc33cc");
-document.documentElement.style.setProperty("--a-color-hover", "#fa3efa");
-document.documentElement.style.setProperty("--footer-background", "#8d72b9");
-document.documentElement.style.setProperty("--news-color", "#b247c7");
-document.documentElement.style.setProperty("--news-border-color", "#994fca");
-document.documentElement.style.setProperty("--aside-color", "#994fca");
+let modeButton= document.querySelector("#mode");
+modeButton.addEventListener("click", ()=>{
+    document.documentElement.style.setProperty("--background", "#627e77");
+    document.documentElement.style.setProperty("--h-color", "orange");
+    document.documentElement.style.setProperty("--section-color", "#363636");
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--h2-color", "orange");
+    document.documentElement.style.setProperty("--a-color", "#cc33cc");
+    document.documentElement.style.setProperty("--a-color-hover", "#fa3efa");
+    document.documentElement.style.setProperty("--footer-background", "#8d72b9");
+    document.documentElement.style.setProperty("--news-color", "#b247c7");
+    document.documentElement.style.setProperty("--news-border-color", "#994fca");
+    document.documentElement.style.setProperty("--aside-color", "#994fca");
+});
