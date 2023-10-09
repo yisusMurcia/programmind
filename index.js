c930aca68ba07ede6a690b0815c89ce332945dd2
yisus <yisusmurci@gmail.com>
2023-10-09 12:34:02 -0500

    header Se diseñó el header tanto para ordenadores como para móviles, añadiendo el código js y css para que funcione correctamente.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-let socialMediaImg= document.getElementsByClassName("socialMediaImg");
-let socialMedia= ["https://twitter.com/theprogrammind", "https://github.com/yisusMurcia", "https://www.threads.net/@jesusantoniomurciaguzman", "https://www.instagram.com/theProgrammind/"];
-let header= document.querySelectorAll(".header");
-let proyectH= header[0];
-let blogH= header[1];
-postDiv= document.querySelector("#proyects");
-let updateNotes= document.getElementById("updateNotes");
-for (let i = 0; i < 4; i++) {
-    socialMediaImg[i].addEventListener("click", ()=>window.open(socialMedia[i]));
-};
-blogH.addEventListener("click", ()=>location.href="blog.html");
-proyectH.addEventListener("click", ()=>location.href="proyectos.html");
-updateNotes.addEventListener("click", ()=>window.open("https://github.com/yisusMurcia/programmind/commits/main"));
+const navbar= document.querySelector("#navbar");
+const openButton= document.querySelector("#open");
+const closeButton= document.querySelector("#close");
+openButton.addEventListener("click", ()=>{
+    navbar.classList.add("visible");
+});
+closeButton.addEventListener("click", ()=>{
+    navbar.classList.remove("visible");
+})
