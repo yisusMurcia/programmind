66b723bb30b19889dbd9deab340f8a31c1c4b31b
yisus <yisusmurci@gmail.com>
2023-10-10 14:23:59 -0500

    Blog -Se terminó el nuevo diseño del blog. -Se añadió un nuevo diseño para los post. -Se redució la cantidad de post que aparecen antes de apretar el boton de expandir, de 6 a 4.

--- blog/index.js
+++ blog/index.js
@@ -8,16 +8,16 @@ let postArray=[
     ["Definiendo el tamaño", "https://programmind.notion.site/Definiendo-el-tama-o-5cf14fa72a2c42799a12a43db6e816ff?pvs=4"],
     ["Agregando imágenes", "https://programmind.notion.site/Im-genes-34d1dcce90c44e4ba2cc6f809996ef4e?pvs=4"],
     ["Inteligencia Artificial", "blogPosts/IA/index.html"],
-    ["El costo de ser bueno", "blogPosts/serBueno/index.html", "blogPosts/serBueno/img.jpg"],
-    ["El sentido de la vida", "blogPosts/sentidoDeLavida/index.html", "blogPosts/sentidoDeLavida/img.jpg"]
+    ["El costo de ser bueno", "../blogPosts/serBueno/index.html", "blogPosts/serBueno/img.jpg"],
+    ["El sentido de la vida", "../blogPosts/sentidoDeLavida/index.html", "blogPosts/sentidoDeLavida/img.jpg"]
 ];
 postArray.reverse();
 let blogContent= document.getElementById("blogContent");
 let addContent= document.querySelector(".moreContent");
-for (let i= 0; i<6; i++){
+for (let i= 0; i<4; i++){
     let post= document.createElement("div");
     post.setAttribute("class", "post");
-    let postTitle= document.createElement("p");
+    let postTitle= document.createElement("h3");
     postTitle.innerText= postArray[i][0];
     post.addEventListener("click", ()=>location.href=(postArray[i][1]));
     post.appendChild(postTitle);
@@ -29,7 +29,7 @@ addContent.addEventListener("click", ()=>{
         for (let post of postArray){
             let postDiv= document.createElement("div");
             postDiv.setAttribute("class", "post");
-            let postTitle= document.createElement("p");
+            let postTitle= document.createElement("h3");
             postTitle.innerText= post[0];
             postDiv.addEventListener("click", ()=>location.href=(post[1]));
             postDiv.appendChild(postTitle);
