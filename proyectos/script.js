43d790fa2e01af0804a2dac3463ca5cf7263d257
yisus <yisusmurci@gmail.com>
2023-10-11 09:42:31 -0500

    Diseño proyectos -Se agregó el nuevo diseño a la página de proyectos. -Se añadieron etiquetas por lenguaje a los post de proyectos.

--- proyectos/script.js
+++ proyectos/script.js
@@ -1,8 +1,8 @@
 let myProyects=[
-    ["Números aleatorios", "proyectos/randomNums/index.html"],
-    ["listas", "proyectos/list/index.html"],
-    ["Cronómetro", "proyectos/chronometer/index.html"],
-    ["IA con Python 🐍", "proyectos/pythonIA/index.html", "proyectos/images/pythonIA.jpg"]
+    ["Números aleatorios", "randomNums/index.html", "Desarrollo web"],
+    ["listas", "list/index.html", "Desarrollo web"],
+    ["Cronómetro", "chronometer/index.html", "Desarrollo web"],
+    ["IA con Python 🐍", "pythonIA/index.html", "Python", "pythonIA/img.jpg"]
 ];
 let button= document.querySelector(".moreContent");
 let proyectsContent= document.querySelector("#myProyects");
@@ -12,25 +12,31 @@ do {
     let post= myProyects[i];
     let postDiv= document.createElement("div");
     postDiv.setAttribute("class", "post");
-    let postTitle= document.createElement("p");
+    let postTitle= document.createElement("h3");
     postTitle.innerText= post[0];
     postDiv.appendChild(postTitle);
+    let postLenguage= document.createElement("p");
+    postLenguage.innerText= post[2];
+    postDiv.appendChild(postLenguage);
     proyectsContent.appendChild(postDiv);
     postDiv.addEventListener("click", ()=>location.href=post[1]);
-    postDiv.style.backgroundImage= `url(${post[2]})`;
+    postDiv.style.backgroundImage= `url(${post[3]})`;
     i++;
-} while (i< 3);
+} while (i< 2);
 button.addEventListener("click", ()=>{
     proyectsContent.innerHTML= "";
     button.remove();
     for (let post of myProyects){
         let postDiv= document.createElement("div");
         postDiv.setAttribute("class", "post");
-        let postTitle= document.createElement("p");
+        let postTitle= document.createElement("h3");
         postTitle.innerText= post[0];
         postDiv.appendChild(postTitle);
+        let postLenguage= document.createElement("p");
+        postLenguage.innerText= post[2];
+        postDiv.appendChild(postLenguage);
         proyectsContent.appendChild(postDiv);
         postDiv.addEventListener("click", ()=>location.href=post[1]);
-        postDiv.style.backgroundImage= `url(${post[2]})`;
+        postDiv.style.backgroundImage= `url(${post[3]})`;
     };
 });
