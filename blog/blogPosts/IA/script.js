f08f50922e98d396a337298665902850f1430fc9
yisus <yisusmurci@gmail.com>
2023-10-13 10:14:58 -0500

    rediseño post del blog

--- blog/blogPosts/IA/script.js
+++
@@ -1,6 +0,0 @@
-let index= document.getElementById("index");
-let blog= document.getElementById("blog");
-let proyects= document.getElementById("proyects");
-index.addEventListener("click", ()=>location.href="../../index.html");
-blog.addEventListener("click", ()=>location.href="../../blog.html");
-proyects.addEventListener("click", ()=>location.href="../../proyectos.html");
