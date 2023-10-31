f1d3dda9e846451229723558bddc874ee4204834
yisus <yisusmurci@gmail.com>
2023-10-30 19:51:48 -0500

    Light mode -Se arreglo el error por el que el color de fondo de las secciónes no correspondía en el modo oscuro, la abreviación del color en hex no funciona correctamente. -Se simplifico el código.

--- header.js
+++ header.js
@@ -29,3 +29,43 @@ const programmindLink= document.querySelector("#programmind");
 programmindLink.addEventListener("click", ()=>{
     changeVisibility("programmindHidden");
 })
+//Modo claro-oscuro
+let modeButton= document.querySelector("#mode");
+let src= modeButton.getAttribute("src");
+//Change the source of the image
+let changeImg=(search, replace)=>{
+        return src.replace(new RegExp(search, "g"), replace);
+};
+//Revisar el tema de acuerdo a al imagen usada
+let checkTheme=()=>{
+    alert(src.match("dark"));
+    if (src.match("dark")==null){
+        return true;
+    };
+    return false;
+};
+modeButton.addEventListener("click", ()=>{
+    if (document.documentElement.style.getPropertyValue("--background")== "#5e8178"){
+        document.documentElement.style.setProperty("--section-color", "111111");
+        document.documentElement.style.setProperty("--background", "#1c1c1c");
+        document.documentElement.style.setProperty("--h-color", "orangered");
+        document.documentElement.style.setProperty("--text-color", "#8591c8");
+        document.documentElement.style.setProperty("--h-color-2", "#4f65ca");
+        document.documentElement.style.setProperty("--a-color", "#cc33cc");
+        document.documentElement.style.setProperty("--a-color-hover", "#fa3efa");
+        document.documentElement.style.setProperty("--news-color", "#8591c8");
+        document.documentElement.style.setProperty("--news-border-color", "#4f65ca");
+        modeButton.setAttribute("src", changeImg("dark","light"));
+    }else{
+        document.documentElement.style.setProperty("--background", "#5e8178");
+        document.documentElement.style.setProperty("--h-color", "orange");
+        document.documentElement.style.setProperty("--section-color", "#c7c7c7");
+        document.documentElement.style.setProperty("--text-color", "1c1c1c");
+        document.documentElement.style.setProperty("--h-color-2", "blue");
+        document.documentElement.style.setProperty("--a-color", "#ddcedd");
+        document.documentElement.style.setProperty("--a-color-hover", "#fa3efa");
+        document.documentElement.style.setProperty("--news-color", "blue");
+        document.documentElement.style.setProperty("--news-border-color", "#994fca");
+        modeButton.setAttribute("src",changeImg("light", "dark"));
+    };
+});
