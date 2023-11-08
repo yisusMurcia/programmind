b87a893dd75b0931957111b815661ac769086f4d
yisus <yisusmurci@gmail.com>
2023-11-07 20:47:30 -0500

    Sin práctica -Se descartó los cambios para incluir las preguntas de programación. -Se corrigió un error por el cual el título de futuros cambios en la sección de práctica no se mostraba como era.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //Añadir preguntas a la sección de práctica de la página principal
+let questDiv= document.querySelector("#questContainer")
+const fecthFile = async () => {
+    let headerFile= "prácticaDeProgramación/index.html"
+    response = await fetch(headerFile);
+  return response.text();
+};
+async function importContent() {
+    const questContent = await fecthFile();
+    document.querySelector("#questContainer").innerHTML = questContent;
+};
+document.addEventListener("DOMContentLoaded", importContent);
