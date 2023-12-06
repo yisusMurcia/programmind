80acd0fcfda57871d8223ea3f713f097e8f79e36
Yisus <yisusmurci@gmail.com>
2023-12-05 21:11:24 -0500

    test de autoamitazación -Se crearon archivos para testear la autamatización de preguntas. -Se creo el código para automatizar preguntas.

--- prácticaDeProgramación/test.js
+++ prácticaDeProgramación/test.js
@@ -1,9 +1,12 @@
-const checkAnswer= (answer, paragraph)=>{
-    let input= document.querySelector(answer);
+const seeAnswer= (obj, paragraph)=>{
+    paragraph.innerHTML= `Respuesta: ${obj.answer+1} <br> ${obj.explicación}`
+}
+const checkAnswer= (answer, text)=>{
+    let input= document.querySelector(`#${answer}`);
     if (input.checked){
-        paragraph.innerHTML= `Correcto`; 
+        text.innerHTML= `Correcto`; 
     }else{
-        paragraph.innerHTML= "Intenta otra vez";
+        text.innerHTML= "Intenta otra vez";
     };
 }
 const programmingConceptsSection= document.querySelector("#questionsProgrammingConcepts");
@@ -19,44 +22,44 @@ let q1= {
     answer: 1,
     explicación: "POO se refiere a una forma de programación basda en el concepto de objetos, una programación orientada a objetos"
 };
-let addQuestion=(section, question)=>{
+let addQuestion=(section, obj)=>{
     const div= document.createElement("div");
     div.setAttribute("class", "codeQuestion");
     const title= document.createElement("h3");
-    title.innerHTML= q1.tema;
+    title.innerHTML= obj.tema;
     div.appendChild(title);
     const questionP= document.createElement("p");
-    questionP.innerHTML= q1.question;
+    questionP.innerHTML= obj.question;
     div.appendChild(questionP)
     const answerContainer= document.createElement("form");
     answerContainer.setAttribute("class", section.getAttribute("class"));
     div.appendChild(answerContainer);
-    for (let i= 0; i< q1.options.length; i++){
+    for (let i= 0; i< obj.options.length; i++){
         const answerFiledset= document.createElement("fieldset");
         const input= document.createElement("input");
         input.setAttribute("type", "radio");
-        input.setAttribute("id", q1.tema+i);
-        input.setAttribute("name", q1.tema);
+        input.setAttribute("id", obj.tema+i);
+        input.setAttribute("name", obj.tema);
         answerFiledset.appendChild(input);
         answerContainer.appendChild(answerFiledset);
         const label= document.createElement("label");
-        label.innerHTML= q1.options[i];
-        label.setAttribute("name", q1.tema);
-        label.setAttribute("for", q1.tema+i);
+        label.innerHTML= obj.options[i];
+        label.setAttribute("name", obj.tema);
+        label.setAttribute("for", obj.tema+i);
         answerFiledset.appendChild(label);
     };
     let paragraph= document.createElement("p");
     paragraph.setAttribute("class", "answer");
-    paragraph.setAttribute("id", `answer${q1.tema}`);
+    paragraph.setAttribute("id", `answer${obj.tema}`);
     paragraph.innerHTML="Trata de responder";
     div.appendChild(paragraph);
     const answerButton= document.createElement("button");
     answerButton.innerHTML="Ver Respuesta";
-    answerButton.addEventListener("click", ()=>{paragraph.innerHTML= q1.explicación});
+    answerButton.addEventListener("click", ()=>{seeAnswer(obj, paragraph)});
     div.appendChild(answerButton);
     const checkAnswerButton= document.createElement("button");
     checkAnswerButton.innerHTML= "Revisar";
-    checkAnswerButton.addEventListener("click", ()=>{checkAnswer(q1.tema+q1.answer, paragraph)})
+    checkAnswerButton.addEventListener("click", ()=>{checkAnswer((obj.tema+obj.answer), paragraph)})
     div.appendChild(checkAnswerButton);
     section.appendChild(div);
 };
