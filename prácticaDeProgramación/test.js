2e019e44640bdbe46f37805dd8a79ac1ef1cf690
Yisus <yisusmurci@gmail.com>
2023-12-06 21:39:47 -0500

    Prueba 2 preguntas -Se realizó un teste con dos preguntas. -El boton de revisar en la segunda pregunta no funciona.

--- prácticaDeProgramación/test.js
+++ prácticaDeProgramación/test.js
@@ -1,32 +1,19 @@
 const seeAnswer= (obj, paragraph)=>{
-    paragraph.innerHTML= `Respuesta: ${obj.answer+1} <br> ${obj.explicación}`
+    paragraph.innerHTML= `Respuesta: ${obj.answer+1} <br> ${obj.explication}`
 }
 const checkAnswer= (answer, text)=>{
     let input= document.querySelector(`#${answer}`);
     if (input.checked){
-        text.innerHTML= `Correcto`; 
+        text.innerHTML= `Correcto`;
     }else{
         text.innerHTML= "Intenta otra vez";
     };
 }
-const programmingConceptsSection= document.querySelector("#questionsProgrammingConcepts");
-let q1= {
-    tema: "POO",
-    question: "¿Qué significa las siglas POO?",
-    options: [
-        "Significa: programación orientada a operaciones",
-        "Significa: programación orientada a objetos",
-        "Significa: procesos de operaciones entre objetos",
-        "Significa: priorización de objetos sobre operaciones"
-    ],
-    answer: 1,
-    explicación: "POO se refiere a una forma de programación basda en el concepto de objetos, una programación orientada a objetos"
-};
 let addQuestion=(section, obj)=>{
     const div= document.createElement("div");
     div.setAttribute("class", "codeQuestion");
     const title= document.createElement("h3");
-    title.innerHTML= obj.tema;
+    title.innerHTML= obj.subject;
     div.appendChild(title);
     const questionP= document.createElement("p");
     questionP.innerHTML= obj.question;
@@ -38,19 +25,19 @@ let addQuestion=(section, obj)=>{
         const answerFiledset= document.createElement("fieldset");
         const input= document.createElement("input");
         input.setAttribute("type", "radio");
-        input.setAttribute("id", obj.tema+i);
-        input.setAttribute("name", obj.tema);
+        input.setAttribute("id", obj.subject+i);
+        input.setAttribute("name", obj.subject);
         answerFiledset.appendChild(input);
         answerContainer.appendChild(answerFiledset);
         const label= document.createElement("label");
         label.innerHTML= obj.options[i];
-        label.setAttribute("name", obj.tema);
-        label.setAttribute("for", obj.tema+i);
+        label.setAttribute("name", obj.subject);
+        label.setAttribute("for", obj.subject+i);
         answerFiledset.appendChild(label);
     };
     let paragraph= document.createElement("p");
     paragraph.setAttribute("class", "answer");
-    paragraph.setAttribute("id", `answer${obj.tema}`);
+    paragraph.setAttribute("id", `answer${obj.subject}`);
     paragraph.innerHTML="Trata de responder";
     div.appendChild(paragraph);
     const answerButton= document.createElement("button");
@@ -59,10 +46,36 @@ let addQuestion=(section, obj)=>{
     div.appendChild(answerButton);
     const checkAnswerButton= document.createElement("button");
     checkAnswerButton.innerHTML= "Revisar";
-    checkAnswerButton.addEventListener("click", ()=>{checkAnswer((obj.tema+obj.answer), paragraph)})
+    checkAnswerButton.addEventListener("click", ()=>{checkAnswer((obj.subject+obj.answer), paragraph)})
     div.appendChild(checkAnswerButton);
     section.appendChild(div);
 };
+const programmingConceptsSection= document.querySelector("#questionsProgrammingConcepts");
+let q2= {
+    subject: "Funciones recursivas",
+    question: "¿Qué significa que una función sea recursiva?",
+    options: [
+        "Una función que no puede terminar",
+        "Una función más eficiente que otra función que hace el mismo trabajo",
+        "Una función basada en la iteración",
+        "Una función que se llama a si misma"
+    ],
+    answer: 1,
+    explication: "Una función recursiva es una función que se ejecuta dentro de esta dada ciertas condiciones."
+};
+let q1= {
+    subject: "POO",
+    question: "¿Qué significa las siglas POO?",
+    options: [
+        "Significa: programación orientada a operaciones",
+        "Significa: programación orientada a objetos",
+        "Significa: procesos de operaciones entre objetos",
+        "Significa: priorización de objetos sobre operaciones"
+    ],
+    answer: 1,
+    explication: "POO se refiere a una forma de programación basda en el concepto de objetos, una programación orientada a objetos"
+};
+addQuestion(programmingConceptsSection, q2);
 addQuestion(programmingConceptsSection, q1);
 /*
 */
