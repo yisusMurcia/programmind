4cff36af50a348ee386ae716de9184a302cdbdb6
Yisus <yisusmurci@gmail.com>
2023-12-10 18:46:33 -0500

    Primeros personajes

---
+++ proyectos/battleTime/script.js
@@ -0,0 +1,58 @@
+//Probabilidad de mejora, entre 0 y 1
+//Boast chance, between 0 and 1
+let getABoast=(num)=>{
+    return Math.random< num;
+};
+class Character{
+    constructor(name, live, damage, critic, criticNum, heal, healNum){
+        this.name= name;
+        this.live= live;
+        this.maxLive= live;
+        this.damage= damage;
+        this.critic= critic;
+        this.criticNum= criticNum;
+        this.heal= heal;
+        this.healNum= healNum;
+    };
+    attack(enemy){
+        let attack= this.damage;
+        if(getABoast(this.criticNum)){
+            attack= this.critic;
+        };
+        enemy.live-= attack;
+    };
+    heal(){
+        let heal= 20;
+        if (getABoast(this.healNum)){
+            heal= this.heal;
+        };
+        this.live+= heal;
+        //La curación no puede sobrepasar la vida máxima
+        //The healing can´t be greater than the max live
+        if (this.live> this.maxLive){
+            this.live= this.maxLive;
+        };
+    };
+};
+//Crear clases para los personajes
+//Create class of the characters
+class Ninja extends Character{
+    constructor(name, live, damage, critic, criticNum, heal, healNum){
+        super("Ninja", 300, 25, 40, 0.25, 45, 0.2);
+    };
+};
+class Gentleman extends Character{
+    constructor(name, live, damage, critic, criticNum, heal, healNum){
+        super("Gentleman", 750, 10, 50, 0.2, 60, 0.7);
+    };
+};
+class Raptor extends Character{
+    constructor(name, live, damage, critic, criticNum, heal, healNum){
+        super("Raptor", 400, 20, 40, 0.25, 45, 0.2);
+    };
+};
+class Caveman extends Character{
+    constructor(name, live, damage, critic, criticNum, heal, healNum){
+        super("Caveman", 600, 15, 20, 0.2, 50, 0.5);
+    };
+};
