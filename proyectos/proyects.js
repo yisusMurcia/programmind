2ca77d09cfe697de980dd7c9f9f0c5960ca1b6fc
Yisus <yisusmurci@gmail.com>
2023-12-12 15:17:14 -0500

    Battle time en proyectos

--- proyectos/proyects.js
+++ proyectos/proyects.js
@@ -2,6 +2,7 @@ let myProyects=[
     ["Números aleatorios", "randomNums/index.html", "Desarrollo web"],
     ["listas", "list/index.html", "Desarrollo web"],
     ["Cronómetro", "chronometer/index.html", "Desarrollo web"],
-    ["IA con Python 🐍", "pythonIA/index.html", "Python", "pythonIA/img.jpg"]
+    ["IA con Python 🐍", "pythonIA/index.html", "Python", "pythonIA/img.jpg"],
+    ["Battle time", "battle/time/index.html", "Desarrollo Web"]
 ];
 myProyects.reverse();
