e15d55453c6740591c602e7f2b1b1ad7201c6ad1
Yisus <yisusmurci@gmail.com>
2023-12-22 16:06:57 -0500

    Cambio de nombre

--- proyectos/proyects.js
+++ proyectos/proyects.js
@@ -2,7 +2,7 @@ let myProyects=[
     ["Números aleatorios", "randomNums/index.html", "Desarrollo web"],
     ["listas", "list/index.html", "Desarrollo web"],
     ["Cronómetro", "chronometer/index.html", "Desarrollo web"],
-    ["IA con Python 🐍", "pythonIA/index.html", "Python", "pythonIA/img.jpg"],
+    ["IA con", "IA/index.html", "Python", "IA/img.jpg"],
     ["Battle time", "battle/time/index.html", "Desarrollo Web", "battleTime/img.jpg"]
 ];
 myProyects.reverse();
