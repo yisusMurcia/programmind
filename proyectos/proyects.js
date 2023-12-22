a7f404137e8b680520c2d74d37ffdb0da6f69464
Yisus <yisusmurci@gmail.com>
2023-12-22 16:24:16 -0500

    Imágenes proyectos

--- proyectos/proyects.js
+++ proyectos/proyects.js
@@ -1,7 +1,7 @@
 let myProyects=[
-    ["Números aleatorios", "randomNums/index.html", "Desarrollo web"],
-    ["listas", "list/index.html", "Desarrollo web"],
-    ["Cronómetro", "chronometer/index.html", "Desarrollo web"],
+    ["Números aleatorios", "randomNums/index.html", "Desarrollo web", "randomNums/img.jpg"],
+    ["listas", "list/index.html", "Desarrollo web", "list/img.jpg"],
+    ["Cronómetro", "chronometer/index.html", "Desarrollo web", "chronometer/img.jpg"],
     ["IA con", "IA/index.html", "Python", "IA/img.jpg"],
     ["Battle time", "battle/time/index.html", "Desarrollo Web", "battleTime/img.jpg"]
 ];
