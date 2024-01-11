e8889a75ee75605840762759778643786d5b9565
Yisus <yisusmurci@gmail.com>
2024-01-11 18:13:26 -0500

    corrección de ruta

--- proyectos/proyects.js
+++ proyectos/proyects.js
@@ -3,6 +3,6 @@ let myProyects=[
     ["listas", "list/index.html", "Desarrollo web", "list/img.jpg"],
     ["Cronómetro", "chronometer/index.html", "Desarrollo web", "chronometer/img.jpg"],
     ["IA con", "IA/index.html", "Python", "IA/img.jpg"],
-    ["Battle time", "battle/time/index.html", "Desarrollo Web", "battleTime/img.jpg"]
+    ["Battle time", "battleTime/index.html", "Desarrollo Web", "battleTime/img.jpg"]
 ];
 myProyects.reverse();
