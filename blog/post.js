a8a8aefdc32188730b240e9a2c16abbc54e06707
Yisus <yisusmurci@gmail.com>
2024-02-02 15:46:48 -0500

    Se removió la antigua base de datos de post y proyectos

--- blog/post.js
+++ blog/post.js
@@ -1,22 +1,3 @@
-let postArray=[
-    ["Los pilares del Desarrollo Web", "desarrollo web", "https://programmind.notion.site/Los-pilares-del-desarrollo-Web-9e36696683884a70a8ac3e450db39c17?pvs=4"],
-    ["Entendiendo HTML", "desarrollo web", "https://programmind.notion.site/Etiquetas-HTML-adcaf805227c4b4db6473cf817b445fd?pvs=4"],
-    ["Primeros pasos en HTML", "desarrollo web", "https://programmind.notion.site/Primeros-pasos-en-HTMl-b712e24ac56d447faf63f7b2131d0c8a?pvs=4"],
-    ["Un poco de estilo", "desarrollo web", "https://programmind.notion.site/Un-poco-de-estilo-76537578429a49f089ff847ffa096e27?pvs=4"],
-    ["Agregando cometarios", "desarrollo web", "https://programmind.notion.site/Comentarios-ec4c2c2062ef4229a38d39270ed191e2?pvs=4"],
-    ["Más que solo párrafos", "desarrollo web", "https://programmind.notion.site/HTML-es-m-s-que-p-rrafos-y-t-tulos-ac7ab7e1ed51448a941b521b6f40d016?pvs=4"],
-    ["Definiendo el tamaño", "desarrollo web", "https://programmind.notion.site/Definiendo-el-tama-o-5cf14fa72a2c42799a12a43db6e816ff?pvs=4"],
-    ["Agregando imágenes", "desarrollo web", "https://programmind.notion.site/Im-genes-34d1dcce90c44e4ba2cc6f809996ef4e?pvs=4"],
-    ["Inteligencia Artificial", "tecnología", "blogPosts/IA/index.html", "blogPosts/IA/img.jpg"],
-    ["El costo de ser bueno", "reflexionar", "blogPosts/serBueno/index.html", "blogPosts/serBueno/img.jpg"],
-    ["El sentido de la vida", "reflexionar", "blogPosts/sentidoDeLavida/index.html", "blogPosts/sentidoDeLavida/img.jpg"],
-    ["La necesidad de ser productivo", "productividad", "blogPosts/productividad/index.html", "blogPosts/productividad/img.jpg"],
-    ["¿Cómo iniciar un día productivo", "productividad", "blogPosts/iniciarElDía/index.html", "blogPosts/iniciarElDía/img.jpg"],
-    ["La lucha por tu atención", "productividad", "blogPosts/luchaPorTuAtención/index.html", "blogPosts/luchaPorTuAtención/img.jpg"],
-    ["Aprovechando el tiempo libre", "productividad", "blogPosts/tiempoLibre/index.html"]
-];
-postArray.reverse();
-
 const blogPosts= [
     {
         id: "tiempo-libre",
--- proyectos/proyects.js
+++ proyectos/proyects.js
@@ -1,13 +1,3 @@
-let myProyects=[
-    ["Números aleatorios", "randomNums/index.html", "Desarrollo web", "randomNums/img.jpg"],
-    ["listas", "list/index.html", "Desarrollo web", "list/img.jpg"],
-    ["Cronómetro", "chronometer/index.html", "Desarrollo web", "chronometer/img.jpg"],
-    ["IA", "IA/index.html", "Python", "IA/img.jpg"],
-    ["Battle time", "battleTime/index.html", "Desarrollo Web", "battleTime/img.jpg"],
-    ["Tres en raya", "tresEnRaya/index.html", "Juego", "tresEnRaya/img.jpg"],
-    ["Conecta cuatro", "conecta4/index.html", "Juego", "conecta4/img.jpeg"]
-];
-myProyects.reverse();
 const projects= [
     {
         id: "conect-four",
